8c5b1a0c0d7f57eb704b9cb979d40c6b96cd30dd
Nomthandazo Ndweni <ndweni2658@gmail.com>
2023-09-06 07:55:17 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        txtFirstName = New Windows.Forms.TextBox()
+        txtSurname = New Windows.Forms.TextBox()
+        txtGender = New Windows.Forms.TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = SystemColors.ControlLightLight
+        Button1.Location = New Point(213, 264)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(225, 82)
+        Button1.TabIndex = 0
+        Button1.Text = "Greet Me!"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' txtFirstName
+        ' 
+        txtFirstName.Location = New Point(213, 69)
+        txtFirstName.Name = "txtFirstName"
+        txtFirstName.Size = New Size(150, 31)
+        txtFirstName.TabIndex = 1
+        ' 
+        ' txtSurname
+        ' 
+        txtSurname.Location = New Point(213, 115)
+        txtSurname.Name = "txtSurname"
+        txtSurname.Size = New Size(150, 31)
+        txtSurname.TabIndex = 1
+        ' 
+        ' txtGender
+        ' 
+        txtGender.Location = New Point(213, 163)
+        txtGender.Name = "txtGender"
+        txtGender.Size = New Size(150, 31)
+        txtGender.TabIndex = 1
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(114, 75)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(59, 25)
+        Label1.TabIndex = 2
+        Label1.Text = "Name"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(114, 121)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(82, 25)
+        Label2.TabIndex = 3
+        Label2.Text = "Surname"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(114, 169)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(69, 25)
+        Label3.TabIndex = 4
+        Label3.Text = "Gender"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(932, 450)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(txtGender)
+        Controls.Add(txtSurname)
+        Controls.Add(txtFirstName)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txtFirstName As Windows.Forms.TextBox
+    Friend WithEvents txtSurname As Windows.Forms.TextBox
+    Friend WithEvents txtGender As Windows.Forms.TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim stFirstName As String
+        Dim stSurname As String
+        Dim stGender As String
+
+        stFirstName = txtFirstName.Text
+        stSurname = txtSurname.Text
+        stGender = txtGender.Text
+
+        MsgBox("Hello " & stFirstName & " " & stSurname & " it is good to know you are " & stGender)
+
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
